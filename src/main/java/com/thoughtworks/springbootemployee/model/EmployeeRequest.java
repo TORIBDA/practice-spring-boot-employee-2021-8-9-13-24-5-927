f08fbc192b3829b3ad661cacd45200597b7a2db9
TORIBDA <david.moses.toribio@oocl.com>
2021-08-13 13:50:01 +0800

    [SpringBoot][TORIBDA] chore: added the EmployeeMapper and EmployeeRequest

---
+++ src/main/java/com/thoughtworks/springbootemployee/model/EmployeeRequest.java
@@ -0,0 +1,73 @@
+package com.thoughtworks.springbootemployee.model;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+public class EmployeeRequest {
+
+    private String name;
+    private Integer age;
+    private String gender;
+    private Integer salary;
+    private Integer company_id;
+
+
+    public EmployeeRequest() {
+    }
+
+    public EmployeeRequest(String name, Integer age, String gender, Integer salary) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.salary = salary;
+    }
+
+    public EmployeeRequest(String name, Integer age, String gender, Integer salary, Integer company_id) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.salary = salary;
+        this.company_id = company_id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public Integer getSalary() {
+        return salary;
+    }
+
+    public Integer getCompany_id() {
+        return company_id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public void setSalary(Integer salary) {
+        this.salary = salary;
+    }
+
+    public void setCompany_id(Integer company_id) {
+        this.company_id = company_id;
+    }
+}
